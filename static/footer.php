5370ba20d11aca557aed453aad196bed47132bf2
Hrishikesh <pawarhrishi21@gmail.com>
2021-04-13 19:36:46 +0530

    Outlline added

---
+++ static/footer.php
@@ -0,0 +1,15 @@
+<?php
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>   
+    <link rel="stylesheet" href="../css/styles.css">
+</head>
+<body>
+    <p class="footer">Copyright © 2021</p>
+</body>
+</html>
