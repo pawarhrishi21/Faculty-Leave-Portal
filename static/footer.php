27f65eff7f4416c24540d21aab65f2633ca377f5
Hrishikesh <pawarhrishi21@gmail.com>
2021-05-04 16:07:30 +0530

    Update footer style

--- static/footer.php
+++ static/footer.php
@@ -1,8 +1,9 @@
 <!-- Footer for all the pages -->
 <?php?>
 
-<div>
-    <p class="footer pt-4">Copyright © 2021</p>
+<div class="footer-div p-3">
+    <p class="footer-p">Copyright © 2021</p>
+    <p><a href="https://github.com/pawarhrishi21/Faculty-Leave-Portal">View on Github</a> </p>
 </div>
 </body>
 </html>
